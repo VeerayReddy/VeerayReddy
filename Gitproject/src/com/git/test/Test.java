596c27f1fe82f4415df6a72b1c8cb26f25f5373f
user <user@DESKTOP-FACGDBB>
2020-04-13 21:29:09 +0530

    Dev1 modiferd Test

--- Gitproject/src/com/git/test/Test.java
+++ Gitproject/src/com/git/test/Test.java
@@ -1,5 +1,10 @@
 package com.git.test;
 
 public class Test {
+	public static void main(String[] args) {
+//		Utility u=new utlility();
+		Test t=new Test();
+		System.out.println(t.hashCode());
+	}
 
 }
